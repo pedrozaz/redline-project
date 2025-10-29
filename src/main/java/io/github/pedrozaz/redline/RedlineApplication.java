d14ac0c310524be72f5dcf4fe5e19f6e7240ee3a
Pedrozaz <guilhermebarb0sa@proton.me>
2025-10-29 18:04:49 -0300

    Initial commit

---
+++ src/main/java/io/github/pedrozaz/redline/RedlineApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedrozaz.redline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/pedrozaz/redline/RedlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedrozaz.redline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
