2ea5d0e8b193faa48b863008b5d6243d6111f5d0
Pedrozaz <guilhermebarb0sa@proton.me>
2025-10-30 13:31:20 -0300

    feat(frontend): Establish app directory structure
    - Creates the foundational folder strucuture inside /src for scalability:
      - /components: for reusable react ui components
      - /hooks: for custom react hooks
      - /services: for low level api client configuration
      - /theme: for chakra ui theme customization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-import { ChakraProvider} from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ChakraProvider } from "@chakra-ui/react"
 
 const queryClient = new QueryClient()
 
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
         <QueryClientProvider client={queryClient}>
 
+            {/*@ts-ignore*/}
             <ChakraProvider>
                 <App />
             </ChakraProvider>
